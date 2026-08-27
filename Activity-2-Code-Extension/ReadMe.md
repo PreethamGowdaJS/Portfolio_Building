7aa068c56fd88fedaeee171c030a1ad861edaa5c
PreethamGowdaJS <preethamgowdasatheesh@gmail.com>
2026-08-27 21:41:16 +0530

    Updated Activity

---
+++ Activity-2-Code-Extension/ReadMe.md
@@ -0,0 +1,3 @@
+Activity 2 code extension
+compiling a java file - javac filename.java
+running a java file - java filename
--- Activity-2/Basic3.java
+++
@@ -1,5 +0,0 @@
-public class Basic3 {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java");
-    }
-}
